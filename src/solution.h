4eb1c99bc7891fc31f709c86282869c1a91f4fef
legobatkata <legobatkata@abv.bg>
2022-12-10 21:09:47 +0200

    initial commit

---
+++ src/solution.h
@@ -0,0 +1,20 @@
+#ifndef TEST2_PREP
+
+struct Node {
+    int key;
+    Node* next;
+    Node(int _key) : key(_key), next(nullptr) {}
+};
+
+
+/*
+int countElements(Node*);
+int countLeafs(Node*);
+int sumElements(Node*);
+int sumElementsAtLevel(Node*, unsigned);
+int height(Node*);
+bool isBST(Node*);
+bool isSymmetric(Node*);*/
+
+#define TEST2_PREP
+#endif //TEST2_PREP
---
+++ test/solution_tests.cpp
@@ -0,0 +1,8 @@
+#include "catch2/catch_all.hpp"
+#include "solution.h"
+
+using namespace std;
+
+TEST_CASE("test case 1") {
+  REQUIRE(true);
+}
