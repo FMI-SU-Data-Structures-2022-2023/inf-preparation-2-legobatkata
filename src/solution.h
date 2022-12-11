1e6511f033135ba78379173589b2f8c120a9aa03
legobatkata <legobatkata@abv.bg>
2022-12-11 19:47:01 +0200

    implemented tasks 1, 2 and 3

--- src/solution.h
+++ src/solution.h
@@ -1,20 +1,26 @@
 #ifndef TEST2_PREP
+#include <iostream>
+#include <string>
 
 struct Node {
-    int key;
+    int data;
     Node* next;
-    Node(int _key) : key(_key), next(nullptr) {}
+    Node(int _data) : data(_data), next(nullptr) {}
 };
 
+struct DoubleNode {
+    int data;
+    DoubleNode* next;
+    DoubleNode* prev;
+    DoubleNode(int _data) : data(_data), next(nullptr), prev(nullptr){}
+};
 
-/*
-int countElements(Node*);
-int countLeafs(Node*);
-int sumElements(Node*);
-int sumElementsAtLevel(Node*, unsigned);
-int height(Node*);
-bool isBST(Node*);
-bool isSymmetric(Node*);*/
+std::string bigNumToString(Node* num);
+std::string delistToString(DoubleNode* start);
+
+Node* sumBigNums(Node* num1, Node* num2);
+Node* mirror(Node* node);
+void doubleMirror(DoubleNode* end);
 
 #define TEST2_PREP
 #endif //TEST2_PREP
